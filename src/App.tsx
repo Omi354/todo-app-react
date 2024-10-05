cf50a761787e8148daed27f13097e2fdec7c7c4c
Omi <bell.tsaeb.shoji+work@gmail.com>
2024-10-06 02:21:24 +0900

    インプットエリアが表示されるようにしました

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import "./App.css";
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider, Button, Flex, Input, Text } from "@chakra-ui/react";
 
 function App() {
+  const handleClick = () => {};
+
   return (
-    <ChakraBaseProvider>
-      <div></div>
-    </ChakraBaseProvider>
+    <ChakraProvider>
+      <div className="container">
+        <Flex justify={"space-between"} gap={"10px"} align={"center"}>
+          <Flex gap={"10px"} align={"center"}>
+            <Text fontSize="sm">タスク名</Text>
+            <Input type="text" placeholder="タスク名を入力" />
+          </Flex>
+          <Flex gap={"10px"} align={"center"}>
+            <Text fontSize="sm">担当名</Text>
+            <Input type="text" placeholder="担当名を入力" />
+          </Flex>
+          <Flex gap={"10px"} align={"center"}>
+            <Text fontSize="sm">期限</Text>
+            <Input type="date" />
+          </Flex>
+          <Button colorScheme="teal" size="sm" onClick={handleClick}>
+            登録
+          </Button>
+        </Flex>
+      </div>
+    </ChakraProvider>
   );
 }
 
